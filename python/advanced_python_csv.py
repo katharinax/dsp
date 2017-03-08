f0a1b99ae1f7415de8117857cbad9682d5388686
Katharina Huang <katharinax@gmail.com>
2017-03-07 18:55:53 -0800

    Complete step 5b CSV

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  7 18:44:13 2017
+
+@author: katharina
+"""
+
+runfile("advanced_python_regex.py")
+
+file = open("emails.csv", "w+")
+for email in email_list:
+    file.write(email)
+    file.write("\n")
+file.close() 
+
